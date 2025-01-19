6474a4e9d4d690f4074a95bbc7fdf55968a0c4b4
NAMAN BERI <124601800+NAMAN-BERI@users.noreply.github.com>
2025-01-19 18:13:37 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 from flask import Flask, request, jsonify
-from flask_cors import CORS
 
 
 import json
 
 app = Flask(__name__)
-CORS(app) 
 # Load the marks data from the JSON file
 with open('marks.json', 'r') as file:
     data = json.load(file)
